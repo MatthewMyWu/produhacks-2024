62b6b427601db6da3bb34af49acb92c66e0ac7e6
dingdongg <cs.dg.kim@gmail.com>
2024-03-24 11:17:43 -0700

    modify logo + serachbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,14 @@ import SearchIcon from "../../public/search_icon.svg";
 export default function Header() {
     return (
         <div className="p-5 flex justify-between bg-white z-10 w-full sticky top-0 left-0 border-b-[1px] border-gray-300">
-            <Image src={Logo} width={40} height={40} alt="Product logo"/>
-            <div className="rounded-full bg-[#e8e8e8] flex">
-                <Image src={SearchIcon} width={14} height={14} alt="Search icon" className="mx-2" />
-                <input type="text" placeholder="Search" className="w-[60vw] rounded-r-full bg-[#e8e8e8] focus:outline-none" />
+            <Image src={Logo} width={76} height={37} alt="Product logo"/>
+            <div className="rounded-full bg-white flex border-[1px] border-black py-3">
+                <Image src={SearchIcon} width={14} height={14} alt="Search icon" className="mx-4" />
+                <input 
+                    type="text" 
+                    placeholder="Search" 
+                    className="w-[60vw] rounded-r-full bg-white focus:outline-none" 
+                />
             </div>
             <Image src={ProfileIcon} width={33} height={33} alt="User profile icon"/>
         </div>
