e6c5bbbd1381272eed4513558c3a0ca5965377fb
dingdongg <cs.dg.kim@gmail.com>
2024-03-24 01:54:48 -0700

    rename filter options

--- src/components/FilterOptions.tsx
+++ src/components/FilterOptions.tsx
@@ -1,11 +1,11 @@
 export default function FilterOptions() {
     return (
-        <div className="bg-purple-600 mb-4 flex">
-            <div className="bg-white mx-4 p-2">filter 1</div>
-            <div className="bg-white mx-4 p-2">filter 2</div>
-            <div className="bg-white mx-4 p-2">filter 3</div>
-            <div className="bg-white mx-4 p-2">filter 4</div>
-            <div className="bg-white mx-4 p-2">filter 5</div>
+        <div className="bg-white mb-4 flex">
+            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Verified</div>
+
+            {/** dropdown menu; recommended + rating */}
+            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Sort</div>
+            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Minimum order quantity</div>
         </div>
     );
 }
