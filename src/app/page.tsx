af2e496466130aec63d5d78fb5b58e192813cec2
dingdongg <cs.dg.kim@gmail.com>
2024-03-24 04:22:43 -0700

    fetch data from server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@ import ResultSection from "../components/ResultSection";
 import Header from "@/components/Header";
 import GoogleMap from "@/components/GoogleMap";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("http://localhost:3000/api");
+  
+  if (res.ok) return res.json();
+  else throw new Error("failed to fetch data");
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
       <div className="flex w-full h-full z-0">
         <ResultSection />
-        <GoogleMap />
+        <GoogleMap suppliers={data} />
       </div>
     </main>
   );
