bfaea730e93aa9acc701c571ae5df4c21428d917
dingdongg <cs.dg.kim@gmail.com>
2024-03-24 02:15:45 -0700

    fix padding

--- src/components/FilterOptions.tsx
+++ src/components/FilterOptions.tsx
@@ -1,11 +1,11 @@
 export default function FilterOptions() {
     return (
-        <div className="bg-white mb-4 flex">
-            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Verified</div>
+        <div className="mb-4 flex">
+            <div className="bg-[#e8e8e8] mx-4 p-2 px-4 rounded-full">Verified</div>
 
             {/** dropdown menu; recommended + rating */}
-            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Sort</div>
-            <div className="bg-[#e8e8e8] mx-4 p-2 rounded-full">Minimum order quantity</div>
+            <div className="bg-[#e8e8e8] mx-4 p-2 px-4 rounded-full">Sort</div>
+            <div className="bg-[#e8e8e8] mx-4 p-2 px-4 rounded-full">Minimum order quantity</div>
         </div>
     );
 }
