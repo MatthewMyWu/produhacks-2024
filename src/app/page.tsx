5c6322c4e4297b4d2d1d3d075f2b4b1946539d1e
MatthewMyWu <matthew6086888@hotmail.com>
2024-03-24 11:47:42 -0700

    Get search context working. Remove homepage wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
+"use client"
 import ResultSection from "../components/ResultSection";
 import Header from "@/components/Header";
 import GoogleMap from "@/components/GoogleMap";
-import {SearchContext, defaultSearchContext} from "@/components/SearchContext";
-import { useState } from "react";
-import HomePage from "@/components/HomePage";
+import { useEffect, useState } from "react";
+import { defaultSearchContext, SearchContext } from "@/components/SearchContext";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api");
-  
-  if (res.ok) return res.json();
-  else throw new Error("failed to fetch data");
-}
+export default function Home() {
+  const [data, setdata] = useState([]);
+  const [searchContext, setSearchContext] = useState(defaultSearchContext);
 
-export default async function Home() {
-  const data = await getData();
+  useEffect(() => {
+    const cb = async () => {
+      const res = await fetch("http://localhost:3000/api", { 
+        method: "GET",
+        cache: "no-store",
+        headers: {
+          "Content-Type": "application/json", // Specify the content type of the body
+        },
+        body: JSON.stringify(searchContext)
+      });
+      const content = await res.json();
+      setdata(content);
+      console.log("from homepage", content);
+    }
+    cb();
+  }, []);
+
   return (
-    <HomePage data={data} />
+    <SearchContext.Provider value = {searchContext} >
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Header />
+      <div className="flex w-full h-full z-0">
+        <ResultSection suppliers={data} />
+        <GoogleMap suppliers={data} />
+      </div>
+    </main>
+    </SearchContext.Provider>
   );
 }
