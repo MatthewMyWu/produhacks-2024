36501ece59bf2ad2150c2cfd3dea3bece8165d2c
dingdongg <cs.dg.kim@gmail.com>
2024-03-24 11:02:59 -0700

    render minimum quantity + rating

--- src/components/StoreList.tsx
+++ src/components/StoreList.tsx
@@ -21,8 +21,10 @@ export default function StoreList({ suppliers }: { suppliers: any[] }) {
                                 className="absolute right-8 bottom-[2.8em]" 
                             />
                         }
+                        <div className="w-10 h-10 leading-10 align-middle text-center text-sm absolute right-8 top-7 rounded-full bg-[#edf738] border border-1 border-black">{ store.rating.toFixed(1) }</div>
                         <Image src="/mock_store_card.png" width={377} height={161} alt="yer" className="mb-4" />
                         <p className="font-bold">{ store.name }</p>
+                        <p className="h-0 text-right text-xs text-gray-300 right-0">Min. order: { store.products[0].minQuantity.toFixed(1) } pieces </p>
                         <p className="text-gray-700">{ store.featuredCategories.join(", ") }</p>
                     </div>
                 ))
